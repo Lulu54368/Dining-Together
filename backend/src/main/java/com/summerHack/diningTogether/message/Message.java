980ced47ba3eef7a80f63be6c78658485c58c032
Lulu <yuntaol@student.unimelb.edu.au>
2022-08-06 11:50:28 +1000

    chat end points

---
+++ backend/src/main/java/com/summerHack/diningTogether/message/Message.java
@@ -0,0 +1,14 @@
+package com.summerHack.diningTogether.message;
+
+
+import lombok.Data;
+
+
+@Data
+public class Message  {
+    private String from;
+    private String to;
+    private String content;
+
+
+}
